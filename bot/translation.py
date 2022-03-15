4ce786be271eb478848a7f9249e16d324548a508
manas1111 <59135271+manas1111@users.noreply.github.com>
2022-03-15 00:49:26 -0700

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -52,5 +52,5 @@ Team SiC 😉  </i>"""
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://t.me/+jzWRm9yMc18xZmQ1">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href=https://t.me/+jzWRm9yMc18xZmQ1>Click Me</a></i>
 """
